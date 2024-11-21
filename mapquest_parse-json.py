188a0a49ae9187a39a0166c67ad902c244d696de
Andre-Roble <152176042+Andre-Roble@users.noreply.github.com>
2024-11-22 01:22:57 +0800

    Create mapquest_parse-json.py
    act 3

---
+++ mapquest_parse-json.py
@@ -0,0 +1,12 @@
+import urllib.parse
+import requests
+
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+orig = "Washington, D.C."
+dest = "Baltimore, Md"
+key = "8d10b93b-97d4-4095-9b54-d964d3969d02" #Replace with your MapQuest key
+
+url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
+
+json_data = requests.get(url).json()
+print(json_data)
